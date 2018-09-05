aeac0776ef9db635d8c1c2cff72e9f0ef1200a09
panjunpeng <panjunpeng@foxmail.com>
2018-09-05 21:34:55 +0800

    项目一第8天：pyg_day08_商品列表&修改&审核&Redis 8. 注解式事务配置

--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/GoodsServiceImpl.java
@@ -14,12 +14,14 @@ import com.pinyougou.vo.Goods;
 import com.pinyougou.vo.PageResult;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.*;
 import java.util.Map.Entry;
 
+@Transactional
 @Service(interfaceClass = GoodsService.class)
 public class GoodsServiceImpl extends BaseServiceImpl<TbGoods> implements GoodsService {
 
@@ -78,6 +80,7 @@ public class GoodsServiceImpl extends BaseServiceImpl<TbGoods> implements GoodsS
     public void addGoods(Goods goods) {
         //1、保存基本信息；在mybatis中如果在保存成功后主键可以回填到保存时候的那个对象中
         goodsMapper.insertSelective(goods.getGoods());
+        
 
         //2、新增商品描述信息
         goods.getGoodsDesc().setGoodsId(goods.getGoods().getId());
@@ -85,53 +88,6 @@ public class GoodsServiceImpl extends BaseServiceImpl<TbGoods> implements GoodsS
 
         //3、保存商品SKU列表
         saveItemList(goods);
-
-
-        //3、新增商品SKU列表
-        /*if (goods.getItemList() != null && goods.getItemList().size() > 0) {
-            for (TbItem item : goods.getItemList()) {
-                String title = goods.getGoods().getGoodsName();
-
-                //组合规格选项形成SKU标题
-                Map<String, Object> map = JSON.parseObject(item.getSpec());
-                Set<Map.Entry<String, Object>> entries = map.entrySet();
-                for (Map.Entry<String, Object> entry : entries) {
-                    title += " " + entry.getValue().toString();
-                }
-
-                item.setTitle(title);
-
-                //图片
-                List<Map> imgList = JSONArray.parseArray(goods.getGoodsDesc().getItemImages(), Map.class);
-                if (imgList != null && imgList.size() > 0) {
-                    //将商品的第一张图作为sku的图片
-                    item.setImage(imgList.get(0).get("url").toString());
-                }
-                //商品分类id
-                item.setCategoryid(goods.getGoods().getCategory3Id());
-                //商品分类名称
-                TbItemCat itemCat = itemCatMapper.selectByPrimaryKey(goods.getGoods().getCategory3Id());
-                item.setCategory(itemCat.getName());
-
-                //创建时间
-                item.setCreateTime(new Date());
-                //更新时间
-                item.setUpdateTime(item.getCreateTime());
-                //SPU商品id
-                item.setGoodsId(goods.getGoods().getId());
-                //商家id
-                item.setSellerId(goods.getGoods().getSellerId());
-                //商家名称
-                TbSeller seller = sellerMapper.selectByPrimaryKey(goods.getGoods().getSellerId());
-                item.setSeller(seller.getName());
-                //品牌名称
-                TbBrand brand = brandMapper.selectByPrimaryKey(goods.getGoods().getBrandId());
-                item.setBrand(brand.getName());
-
-                itemMapper.insertSelective(item);
-
-            }
-        }*/
     }
 
     /**
