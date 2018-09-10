671531acbfc8b43888718378f111396e992e0c4a
panjunpeng <panjunpeng@foxmail.com>
2018-09-10 19:00:46 +0800

    项目一第11天：pyg_day11_搜索系统-过滤&分页&排序&更新索引 4. 按价格区间筛选

--- pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
+++ pinyougou/pinyougou-search-web/src/main/webapp/js/controller/searchController.js
@@ -8,11 +8,11 @@ app.controller("searchController", function ($scope, searchService) {
     };
 
     //搜索对象
-    $scope.searchMap = {"keywords":"", "category":"", "brand":"", "spec":{}};
+    $scope.searchMap = {"keywords":"", "category":"", "brand":"", "spec":{}, "price":""};
 
     //添加过滤条件
     $scope.addSearchItem = function (key, value) {
-        if ("brand" == key || "category" == key) {
+        if ("brand" == key || "category" == key || "price" == key) {
             //如果点击的是品牌或者分类的话
             $scope.searchMap[key] = value;
         } else {
@@ -25,7 +25,7 @@ app.controller("searchController", function ($scope, searchService) {
 
     //撤销过滤条件
     $scope.removeSearchItem = function (key) {
-        if ("brand" == key || "category" == key) {
+        if ("brand" == key || "category" == key || "price" == key) {
             //如果点击的是品牌或者分类的话
             $scope.searchMap[key] = "";
         } else {
--- pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -1,23 +1,23 @@
 package com.pinyougou.search.service.impl;
 
+
 import com.alibaba.dubbo.config.annotation.Service;
 import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.HighlightOptions;
-import org.springframework.data.solr.core.query.SimpleHighlightQuery;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
 import org.springframework.data.solr.core.query.result.HighlightEntry;
 import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
+import org.springframework.util.StringUtils;
 
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @Service(interfaceClass = ItemSearchService.class)//为了引用事务才说明类，这里可以不用
 public class ItemSearchServiceImpl implements ItemSearchService {
@@ -44,6 +44,51 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
 
+        //按照分类过滤
+        if (!StringUtils.isEmpty(searchMap.get("category"))) {
+            Criteria categoryCriteria = new Criteria("item_category").is(searchMap.get("category"));
+            SimpleFilterQuery categoryFilterQuery = new SimpleFilterQuery(categoryCriteria);
+            query.addFilterQuery(categoryFilterQuery);
+
+
+        }
+
+        //按照品牌过滤
+        if (!StringUtils.isEmpty(searchMap.get("brand"))) {
+            Criteria brandCriteria = new Criteria("item_brand").is(searchMap.get("brand"));
+            SimpleFilterQuery brandFilterQuery = new SimpleFilterQuery(brandCriteria);
+            query.addFilterQuery(brandFilterQuery);
+        }
+
+        //按照规格过滤
+        if (searchMap.get("spec") != null) {
+            Map<String, String> specMap = (Map<String, String>) searchMap.get("spec");
+            Set<Map.Entry<String, String>> entrySet = specMap.entrySet();
+            for (Map.Entry<String, String> entry : entrySet) {
+                Criteria specCriteria = new Criteria("item_spec_" + entry.getKey()).is(entry.getValue());
+                SimpleFilterQuery specFilterQuery = new SimpleFilterQuery(specCriteria);
+                query.addFilterQuery(specFilterQuery);
+            }
+        }
+
+        //按照价格区间过滤
+        if (!StringUtils.isEmpty(searchMap.get("price"))) {
+            //获取起始、结束价格
+            String[] prices = searchMap.get("price").toString().split("-");
+
+            //价格大于等于起始价格
+            Criteria startPriceCriteria = new Criteria("item_price").greaterThanEqual(prices[0]);
+            SimpleFilterQuery startPriceFilterQuery = new SimpleFilterQuery(startPriceCriteria);
+            query.addFilterQuery(startPriceFilterQuery);
+
+            //价格小于等于结束价格
+            if (!"*".equals(prices[1])) {
+                Criteria endPriceCriteria = new Criteria("item_price").lessThanEqual(prices[1]);
+                SimpleFilterQuery endPriceFilterQuery = new SimpleFilterQuery(endPriceCriteria);
+                query.addFilterQuery(endPriceFilterQuery);
+            }
+        }
+
         //设置高亮域
         HighlightOptions highlightOptions = new HighlightOptions();
         //高亮域名称
@@ -56,18 +101,18 @@ public class ItemSearchServiceImpl implements ItemSearchService {
 
         query.setHighlightOptions(highlightOptions);
 
-
         //查询
         HighlightPage<TbItem> highlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
 
         //获取高亮标题
         List<HighlightEntry<TbItem>> highlighted = highlightPage.getHighlighted();
 
-        //对每个商品的标题获取高亮标题并回填
-
-        for (HighlightEntry<TbItem> entry : highlighted) {
-            if (entry.getHighlights() != null && entry.getHighlights().get(0).getSnipplets() != null) {
-                entry.getEntity().setTitle(entry.getHighlights().get(0).getSnipplets().get(0));
+        if (highlighted != null && highlighted.size() > 0) {
+            //对每个商品的标题获取高亮标题并回填
+            for (HighlightEntry<TbItem> entry : highlighted) {
+                if (entry.getHighlights() != null && entry.getHighlights().size() > 0) {
+                    entry.getEntity().setTitle(entry.getHighlights().get(0).getSnipplets().get(0));
+                }
             }
         }
 
