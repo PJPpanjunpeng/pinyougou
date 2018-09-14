737b0ef9dbf2db7638ccacdfe7e228a80732b3b2
panjunpeng <panjunpeng@foxmail.com>
2018-09-14 22:24:59 +0800

    项目一第14天：pyg_day14_SpringBoot&短信微服务&用户注册 3.3. 发送短信测试--未测试 4. 短信微服务--未测试

--- itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/MQController.java
+++ itcast-spring-boot/src/main/java/cn/itcast/springboot/controller/MQController.java
@@ -26,4 +26,21 @@ public class MQController {
 
         return "发送消息完成";
     }
+
+
+    /**
+     * 发送一个手机短信消息到 MQ 的队列
+     * @return
+     */
+    @GetMapping("/sendSms")
+    public String sendSmsMsg() {
+        Map<String, String> map = new HashMap<>();
+        map.put("mobile", "手机号");
+        map.put("signName", "黑马");
+        map.put("templateCode", "SMS_125018593");
+        map.put("templateParam", "{\"code\":\"123456\"}");
+        jmsMessagingTemplate.convertAndSend("itcast_sms_queue", map);
+
+        return "发送 sms 消息完成。 ";
+    }
 }
